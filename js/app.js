6930c55d8451a305f16a8f095176015917ee982a
leeburgess67 <leeburgessmail@gmail.com>
2018-08-03 15:32:59 +0100

    delete all button added

--- js/app.js
+++ js/app.js
@@ -1,49 +1,48 @@
+// When DOM loaded, do everything below
 document.addEventListener('DOMContentLoaded', () => {
   console.log('JavaScript loaded');
 
+  //Target the form using the ID and assign it to form variable
   const form = document.querySelector("#new-item-form")
+
+  // Create a ul and assign to variable
   const toDoList = document.createElement("ul")
+
+  // Save the location you want the item to go to a variable. #todo-list is the ID of the div at the bottom of the page
   const toDoDiv = document.querySelector("#todo-list")
+
+  // Append the child (ul = toDoList) to the parent (the div - toDoDiv)
   toDoDiv.appendChild(toDoList);
 
+  // Add addEventListener to the form, when form is submitted, do the callback function
+  form.addEventListener('submit', (event) => {
 
-  form.addEventListener('submit' (event) => {
+  // Prevent the default activity (in this case submit as it is a form)
   event.preventDefault();
 
-
+  // Assign the input form values to a variable - what, who and when are the input IDs in the form
   const whatToDo = event.target.what.value;
   const whoToDo = event.target.who.value;
   const whenToDo = event.target.when.value;
 
+  //Create a new li element and assign to variable
   const toDoItem = document.createElement("li");
 
-  toDoItem.textContent = `${whatToDo}, ${whoToDo}, ${whenToDo}`
-  toDoList.appendChild(toDoItem)
+  // Define the toDoItem text content using the variables set above
+  toDoItem.textContent = `What: ${whatToDo}, Who: ${whoToDo}, When: ${whenToDo}`
+
+  // Append the todo item (li) item to the list (ul)
+  toDoList.appendChild(toDoItem);
+
+  //Resets the form after each input
+  form.reset();
 
 
-})
+  const deleteButton = document.querySelector("#button");
+  deleteButton.addEventListener("click", () => {
+    console.dir(deleteButton);
+    toDoList.innerHTML = '';
+});
+
+});
   });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-})
